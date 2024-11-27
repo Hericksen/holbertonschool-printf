51b741efb41fff4a69c4e515b5502d5cb9e48e8e
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-27 15:04:15 +0100

    Betty fix

--- main.h
+++ main.h
@@ -5,17 +5,26 @@
 #include <stddef.h>
 #include <stdarg.h>
 #include <stdlib.h>
-
+/**
+* struct specifier - Struct that maps format specifiers to corresponding
+* functions.
+* @type: A pointer to a character string representing a format specifier
+* (e.g., "c" or "s").
+* @func_print: A function pointer to the corresponding printing function
+*/
 typedef struct specifier
 {
-    char *type;
-    int (*func_print)(va_list list);
+	char *type;
+
+	int (*func_print)(va_list list);
 } specifier_t;
 
 int _putchar(char c);
+
 int _printf(const char *format, ...);
+
 int print_char(va_list list);
 int print_string(va_list list);
 int print_percent(va_list list);
 int print_decimal(va_list list);
-#endif
+#endif /* MAIN_H */
