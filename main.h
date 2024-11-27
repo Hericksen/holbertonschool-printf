6a5660379575b6bf4412b583de78ebf2ca6a5ba3
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-27 13:29:13 +0100

    Update project

--- main.h
+++ main.h
@@ -8,12 +8,13 @@
 typedef struct specifier
 {
     char *type;
-    int (*f)(va_list ap);
+    int (*func_print)(va_list list);
 } specifier_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list list);
 int print_string(va_list list);
-int(*get_format(char c));
+int print_percent(va_list list);
+int print_decimal(va_list list);
 #endif
--- printf_project.c
+++ printf_project.c
@@ -1,43 +1,44 @@
 #include "main.h"
-
+#include <stdio.h>
 int _printf(const char *format, ...)
 {
     specifier_t spec[] = {
         {"c", print_char},
         {"s", print_string},
+        {"%", print_percent},
+        {"d", print_decimal},
+        {"i", print_decimal},
         {NULL, NULL},
     };
-    va_list ap;
-    int i = 0, j = 0;
-    int count;
-    va_start(ap, format);
+    va_list list;
+    int i = 0, j = 0, count;
+    va_start(list, format);
 
     while (format && format[i] != '\0')
     {
         if (format[i] == '%')
         {
             i++;
-            if(format[i] == '%')
-                _putchar(37);
             while (spec[j].type)
             {
                 if (*spec[j].type == format[i])
                 {
-                    spec[j].f(ap);
+                    spec[j].func_print(list);
                     count++;
+                    break;
                 }
                 j++;
             }
+
         }
-        else if (format[i] == '%')
-            _putchar('%');
         else
         {
             _putchar(format[i]);
             count++;
         }
+        j = 0;
         i++;
     }
-    va_end(ap);
+    va_end(list);
     return (count);
 }
