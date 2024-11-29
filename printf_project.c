437abac134f580ebf287a10b59410440fe2d9814
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-29 10:58:49 +0100

    Update Printf

--- printf_project.c
+++ printf_project.c
@@ -1,15 +1,42 @@
 #include "main.h"
 /**
+ * process_specifier - Processes a format specifier and calls the appropriate
+ * function to handle it.
+ * @format: A string containing the format specifier.
+ * @list: The va_list with arguments.
+ * @count: A pointer to the count of characters printed.
+ * Return: 1 if the specifier was valid, 0 otherwise.
+ */
+int process_specifier(const char *format, va_list list, int *count)
+{
+	specifier_t spec[] = {{"c", print_char},
+	{"s", print_string},
+	{"%", print_perc},
+	{"d", print_deci},
+	{"i", print_deci},
+	{NULL, NULL}};
+	int j = 0;
+
+	while (spec[j].type)
+	{
+		if (*spec[j].type == *format)
+		{
+			*count += spec[j].func_print(list);
+			return (1);
+		}
+		j++;
+	}
+	return (0);
+}
+/**
  * _printf - Custom implementation of the printf function.
  * @format: A string containing characters to print and format specifiers.
  * Return: The total number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	specifier_t spec[] = {{"c", print_char}, {"s", print_string},
-	{"%", print_perc}, {"d", print_deci}, {"i", print_deci}, {NULL, NULL},};
 	va_list list;
-	int i = 0, j = 0, count = 0, verif;
+	int i = 0, count = 0;
 
 	va_start(list, format);
 	if (format == NULL)
@@ -19,21 +46,19 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			verif = 1;
-			while (spec[j].type)
+			/* Skip spaces after '%'*/
+			while (format[i] == ' ')
+				i++;
+			if (!process_specifier(&format[i], list, &count))
 			{
-				if (*spec[j].type == format[i])
+				/* Handle invalid specifier*/
+				_putchar('%');
+				if (format[i] != '\0')
 				{
-					count += spec[j].func_print(list);
-					verif = 0;
+					_putchar(format[i]);
+					count++;
 				}
-				j++;
-			}
-			if (verif == 1)
-			{
-				_putchar(format[i - 1]);
-				_putchar(format[i]);
-				count += 2;
+				count++;
 			}
 		}
 		else
@@ -41,7 +66,6 @@ int _printf(const char *format, ...)
 			_putchar(format[i]);
 			count++;
 		}
-		j = 0;
 		i++;
 	}
 	va_end(list);
